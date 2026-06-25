1c88c6a7a3963f19893ce4a5cc79e8cae8cc4896
Charles Alves <charles.siqueira@gmail.com>
2026-06-25 17:11:40 -0300

    feat: criando módulo de domínio

---
+++ src/domain/app_config.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use crate::infra::project_files;
+
+#[derive(Serialize, Deserialize)]
+pub struct AppConfig {
+    pub jira: Option<String>,
+    pub git: Option<String>,
+    pub workspace: PathBuf,
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        let workspace = project_files::workspace()
+            .expect("Não foi possível identificar o Workspace");
+        Self {
+            jira: None,
+            git: None,
+            workspace,
+        }
+    }
+}
+
