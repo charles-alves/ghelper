21cfc5f83a6c993fd30114a26b76898b00300115
Charles Alves <charles.siqueira@gmail.com>
2026-06-26 12:30:38 -0300

    feat: criando view para exibição de select

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -1,3 +1,4 @@
 pub mod config_form_view;
 pub mod input;
 pub mod confirm;
+pub mod select;
---
+++ src/view/select.rs
@@ -0,0 +1,15 @@
+use anyhow::{Context, Result};
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::Select;
+
+pub fn render(prompt: &str, itens: Vec<String>, default: Option<usize>, theme: &ColorfulTheme) -> Result<usize> {
+    let mut confirm = Select::with_theme(theme)
+        .with_prompt(prompt);
+    if let Some(default) = default {
+        confirm = confirm.default(default);
+    }
+    confirm.items(itens)
+        .max_length(2)
+        .interact()
+        .context("Não foi possível confimar o resultado")
+}
