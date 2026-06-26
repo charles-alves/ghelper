3e0399efab64cf7228b0389bf7bf5aa1ef063741
Charles Alves <charles.siqueira@gmail.com>
2026-06-26 13:12:13 -0300

    feat: melhorando view select para aceitar um valor em vez de posição como valor default

--- src/view/select.rs
+++ src/view/select.rs
@@ -2,13 +2,12 @@ use anyhow::{Context, Result};
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Select;
 
-pub fn render(prompt: &str, itens: &Vec<String>, default: Option<usize>, theme: &ColorfulTheme) -> Result<usize> {
-    let mut confirm = Select::with_theme(theme)
-        .with_prompt(prompt);
-    if let Some(default) = default {
-        confirm = confirm.default(default);
-    }
-    confirm.items(itens)
+pub fn render(prompt: &str, itens: &Vec<String>, default: Option<String>, theme: &ColorfulTheme) -> Result<usize> {
+    let index = default.and_then(|d| itens.iter().position(|i| i == &d));
+    Select::with_theme(theme)
+        .with_prompt(prompt)
+        .default(index.unwrap_or(0))
+        .items(itens)
         .max_length(2)
         .interact()
         .context("Não foi possível confimar o resultado")
