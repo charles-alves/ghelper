8d4dadfdefd06aff54f8646baf148ad9fdb45277
Charles Alves <charles.siqueira@gmail.com>
2026-06-26 00:09:23 -0300

    feat: permitindo que os valores fiquem vazios no formulário de configuração

--- src/view/config_form_view.rs
+++ src/view/config_form_view.rs
@@ -7,11 +7,11 @@ use crate::view::input;
 
 pub fn render(app_config: &mut AppConfig) -> Result<()> {
     let theme = ColorfulTheme::default();
-    let input_jira = input::render("URL base do Jira", app_config.jira.as_deref(), &theme);
-    app_config.jira = Some(input_jira.interact_text()?);
-    let input_git = input::render("URL base do Git", app_config.git.as_deref(), &theme);
-    app_config.git = Some(input_git.interact_text()?);
-    let input_workspace = input::render("Diretório base para armazenamento dos projetos", app_config.workspace.to_str(), &theme);
-    app_config.workspace = PathBuf::from(input_workspace.interact_text()?);
+    let input_jira = input::render("URL base do Jira", app_config.jira.as_deref(), &theme)?;
+    app_config.jira = input_jira;
+    let input_git = input::render("URL base do Git", app_config.git.as_deref(), &theme)?;
+    app_config.git = input_git;
+    let input_workspace = input::render("Diretório base para armazenamento dos projetos", app_config.workspace.to_str(), &theme)?;
+    app_config.workspace = input_workspace.map(PathBuf::from).unwrap();
     Ok(())
 }
--- src/view/input.rs
+++ src/view/input.rs
@@ -1,11 +1,17 @@
+use anyhow::Result;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Input;
 
-pub fn render<'a>(prompt: &'a str, default: Option<&'a str>, theme: &'a ColorfulTheme) -> Input<'a, String> {
-    let input = Input::<String>::with_theme(theme)
+pub fn render(prompt: &str, default: Option<&str>, theme: &ColorfulTheme) -> Result<Option<String>> {
+    let mut input = Input::<String>::with_theme(theme)
         .with_prompt(prompt);
     if let Some(default) = default {
-        return input.default(default.to_string());
+        input = input.default(default.to_string());
     }
-    input
+    let value = input.allow_empty(true).interact_text()?;
+    if value.trim().is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(value))
+    }
 }
